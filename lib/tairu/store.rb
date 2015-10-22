cf1914afb50df01d07f51ca20eac9b264fca6bc3
slongberry <slongberry@sanborn.com>
2015-10-22 09:45:13 -0600

    Added new store type to store config

--- lib/tairu/store.rb
+++ lib/tairu/store.rb
@@ -2,6 +2,7 @@ require 'store/mbtiles'
 require 'store/esri'
 require 'store/tms'
 require 'store/geojson'
+require 'store/zxy'
 
 module Tairu
   module Store
@@ -9,7 +10,8 @@ module Tairu
       'mbtiles' => Tairu::Store::MBTiles,
       'esri' => Tairu::Store::Esri,
       'tms' => Tairu::Store::TMS,
-      'json' => Tairu::Store::GeoJSON
+      'json' => Tairu::Store::GeoJSON,
+      'zxy' => Tairu::Store::ZXY
     }
   end
 end
